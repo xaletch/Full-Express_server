d626b466dfe9d8be55dfe4383c3f93b15541233b
Kirill <125193708+xaletch@users.noreply.github.com>
2024-05-23 00:35:31 +0300

    server

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 88;
 
-app.use(express.static("Full-Express_client/dist"));
+app.use(express.static("../Full-Express_client/dist"));
 
 app.listen(port, () => console.log("Сервер запущен на порту: ", port));
